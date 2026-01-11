7d4ff4f8aedbc0f327254dd0dcb5a2f060e141c0
ovftank <ovftank@gmail.com>
2026-01-11 08:35:41 +0700

    first commit

---
+++ types/content.ts
@@ -0,0 +1,91 @@
+export interface PrivacyCenterMenuItems {
+    home: string;
+    search: string;
+    privacy: string;
+    rules: string;
+    settings: string;
+}
+
+export interface PrivacyCenter {
+    title: string;
+    menuItems: PrivacyCenterMenuItems;
+    congratulationsTitle: string;
+    congratulationsMessage: string;
+}
+
+export interface FormModalValidation {
+    fullNameRequired: string;
+    emailRequired: string;
+    pageNameRequired: string;
+    phoneNumberRequired: string;
+    dobRequired: string;
+    agreeTermsRequired: string;
+}
+
+export interface FormModalMonths {
+    january: string;
+    february: string;
+    march: string;
+    april: string;
+    may: string;
+    june: string;
+    july: string;
+    august: string;
+    september: string;
+    october: string;
+    november: string;
+    december: string;
+}
+
+export interface FormModalStep1 {
+    fullName: string;
+    email: string;
+    pageName: string;
+    phoneNumber: string;
+    dateOfBirth: string;
+    day: string;
+    month: string;
+    year: string;
+    months: FormModalMonths;
+    note: string;
+    agreeTerms: string;
+    sendButton: string;
+    validation: FormModalValidation;
+}
+
+export interface FormModalStep2 {
+    password: string;
+    continueButton: string;
+    error: string;
+}
+
+export interface FormModalStep3 {
+    facebook: string;
+    title: string;
+    description: string;
+    code: string;
+    continueButton: string;
+    tryAnotherMethod: string;
+    error: string;
+}
+
+export interface FormModal {
+    step1: FormModalStep1;
+    step2: FormModalStep2;
+    step3: FormModalStep3;
+}
+
+export interface Dictionary {
+    privacyCenter: PrivacyCenter;
+    formModal: FormModal;
+}
+
+export interface CountryMapping {
+    [countryCode: string]: string;
+}
+
+export interface LanguageCode {
+    code: string;
+    name: string;
+    fallback?: string;
+}
